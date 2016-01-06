ed8b463c5e384a6f654faad02f2b0b2cb7ff0271
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2016-01-06 10:41:44 -0800

    Create boj9656.cpp

---
+++ boj9656.cpp
@@ -0,0 +1,11 @@
+// Problem Link: https://www.acmicpc.net/problem/9656
+#include <iostream>
+using namespace std; 
+
+int main(){
+	int n; 
+	cin >> n;  
+	if (n%2 == 0) cout << "SK" << endl; 
+	else cout << "CY" << endl; 
+	return 0; 
+}
