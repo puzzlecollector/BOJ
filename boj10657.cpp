d05f61c0531dd5e80b07152d876f7f1bfd597b43
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2016-02-17 14:56:54 -0800

    Create boj10657.cpp

---
+++ boj10657.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include <cstdlib> 
+#include <cstdio> 
+#include <algorithm> 
+using namespace std; 
+
+typedef long long ll; 
+ll a[100001];  
+
+int main(){
+	int n,group = 1; 
+	ll pos;  
+	cin >> n; 
+	for (int i = 0; i < n; i++){
+		cin >> pos >> a[i];  
+	}
+	ll comp = a[n-1];  
+	for (int i = n-2; i >= 0; i--){
+		if (a[i] > comp){
+			continue;  // cows group 
+		}else{
+			++group; 
+		}
+		comp = min(comp,a[i]);  
+	}
+	cout << group << endl; 
+	return 0; 
+}
