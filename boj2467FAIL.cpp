3c6a80f1444521c177c10ae546c15fcff72fc708
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2016-04-15 00:28:59 -0700

    Create boj2467FAIL.cpp

---
+++ boj2467FAIL.cpp
@@ -0,0 +1,45 @@
+#include <iostream> 
+#include <cstdio> 
+#include <cstdlib> 
+#include <algorithm> 
+#include <climits> 
+#include <vector> 
+using namespace std;  
+
+int a[100001];
+int N; 
+
+int binarySearch(int l, int r, int find){
+	int idx;  
+	while (r - l >= 1){
+		int mid = (l+r)/2; 
+		if (a[mid] == find) return mid; 
+		else if (a[mid] < find) l = mid+1; 
+		else r = mid;  
+		idx = mid; 
+	}
+	return idx; 
+}
+
+int main(){
+	scanf("%d",&N);
+	for (int i = 0; i < N; i++){
+		scanf("%d",&a[i]); 
+	}  
+	int closest = INT_MAX;  
+	int pos1,pos2;  
+	// O(nlog(n))
+	for (int i = 0; i < N; i++){
+		int pos = binarySearch(0,N,-a[i]); 
+		if (closest > abs(a[i]+a[pos])){
+			closest = abs(a[i]+a[pos]); 
+			if (a[i] < a[pos]){
+				pos1 = i, pos2 = pos; 
+			}else{
+				pos1 = pos, pos2 = i; 
+			}
+		}
+	}	
+	printf("%d %d\n",a[pos1],a[pos2]); 
+	return 0; 
+}
