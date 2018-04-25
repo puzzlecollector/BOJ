439310e40b7b6bbcad3278a9edefb427fdc5d0de
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-04-25 12:20:50 -0700

    Create boj2166.cpp

---
+++ boj2166.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+using namespace std; 
+struct point{
+	int x,y; 
+};  
+point a[10005]; 
+int main(){
+	int n; 
+	cin >> n; 
+	for (int i = 0; i < n; i++){
+		cin >> a[i].x >> a[i].y;  
+	}
+	double area = 0; 
+	int j = n-1;  
+	for (int i = 0; i < n; i++){
+		area += (a[j].x+a[i].x)*(a[j].y-a[i].y); 
+		j = i; 
+	}
+	cout.setf(ios::fixed); 
+	cout.setf(ios::showpoint); 
+	cout.precision(1); 
+	cout << area/2 << endl;
+	return 0; 
+}
