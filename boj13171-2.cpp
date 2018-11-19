812a37b2b392c45a7366a82955ccbfb2bdd9a9ea
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-11-19 10:49:34 -0800

    Create boj13171-2.cpp

---
+++ boj13171-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std; 
+typedef long long ll;  
+const ll mod = (ll)1e9+7;  
+int main(){
+	ll A[64] = {1,}, X, res = 1LL;  
+	cin >> A[1] >> X;  
+	A[1] %= mod;  
+	for (int i = 2; i < 64; i++){
+		A[i] = (1LL*A[i-1]*A[i-1])%mod;  
+	}
+	for (int i = 0; i < 63; i++){
+		if (X>>i & 1LL){
+			res = (1LL*res*A[i+1])%mod; 
+		}
+	}
+	cout << res << endl; 
+	return 0; 
+}
