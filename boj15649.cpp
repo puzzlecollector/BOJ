c808ee1a9c7ad097c4b5b4dbdc06374431deaef0
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-11-19 11:38:04 -0800

    Update boj15649.cpp

--- boj15649.cpp
+++ boj15649.cpp
@@ -1,55 +1,58 @@
-// timeout - failed code 
+// accepted 
 #include <iostream>
 #include <cstdlib>
 #include <algorithm>
 #include <vector> 
 #include <set> 
 using namespace std; 
-void permute(string s,int l,int r,set<string> &st){
+void permute(string s,int l,int r,vector<string> &ans){
 	if (l == r){
-		st.insert(s);  
+		ans.push_back(s);  
 		return; 
 	}
 	for (int i = l; i <= r; i++){
 		swap(s[i],s[l]); 
-		permute(s,l+1,r,st); 
+		permute(s,l+1,r,ans); 
 		swap(s[i],s[l]); 
 	}
 }
+int N,M; 
+vector<string> v;  
+void combination(int k,string s,int past){
+	if (k == M){
+		v.push_back(s); 
+		return; 
+	}
+	if (past == -1){
+		for (int i = 1; i<= N; i++){
+			s += string(1,char(i+'0'));  
+			combination(k+1,s,i);  
+			s.pop_back(); 
+		}
+	}else{
+		for (int i = past+1; i <= N; i++){
+			s += string(1,char(i+'0'));  
+			combination(k+1,s,i);  
+			s.pop_back();
+		}
+	}
+}
 int main(){
-	int N,M; 
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);  
 	cin >> N >> M;  
 	string s; 
-	for (int i = 0; i < N-M; i++){
-		s += "0"; 
-	}
-	for (int i = 0; i < M; i++){
-		s += "1"; 
-	}
-	set<string> st; 
-	permute(s,0,s.size()-1,st); 
+	// N choose M 
+	combination(0,s,-1); 
 	vector<string> ans; 
-	for (set<string>::iterator it = st.begin(); it != st.end(); it++){
-		string t = *it; 
-		string L;  
-		for (int j = 0; j < t.size(); j++){
-			if ((t[j]-'0')&1){
-				L += string(1,char(j+1+'0'));  
-			}
-		}
-		set<string> f;  
-		permute(L,0,L.size()-1,f); 
-		for (set<string>::iterator it = f.begin(); it != f.end(); it++){
-			ans.push_back(*it); 
-		}
+	for (int i = 0; i < v.size(); i++){
+		permute(v[i],0,v[i].size()-1,ans); 
 	}
 	sort(ans.begin(),ans.end()); 
 	for (int i = 0; i < ans.size(); i++){
-		for (int j = 0 ;j < ans[i].size(); j++){
-			cout << ans[i][j]; 
-			if (j == ans[i].size()-1) cout << endl; 
-			else cout << " "; 
+		for (int j = 0; j < ans[i].size(); j++){
+			cout << ans[i][j] << " "; 
 		}
+		cout << "\n"; 
 	}
 	return 0; 
 }
